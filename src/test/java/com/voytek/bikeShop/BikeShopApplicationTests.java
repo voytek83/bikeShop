b64483b641b522e638334f38918247a81bcdfe58
Wojciech Wysocki <voytek83@gmail.com>
2021-10-20 08:34:05 +0200

    6th Changed method to calculate price separate from db operation, to allow unit test of it. Written one assertEquals test for it

--- src/test/java/com/voytek/bikeShop/BikeShopApplicationTests.java
+++ src/test/java/com/voytek/bikeShop/BikeShopApplicationTests.java
@@ -2,17 +2,23 @@ package com.voytek.bikeShop;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class BikeShopApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private Controller controller;
 
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
